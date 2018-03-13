3779a94da69dab51995c96971e7dd1cee6766f2c
Iza Dziura <izadziura@wp.pl>
2018-03-13 09:25:36 +0100

    Add christmas tree

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var drawTree = prompt('Podaj wysokość');
 
 for (var i = 0; i < drawTree; i++) {
-	var star = '';
+	var star = '*';
 	var space = '';
 
-	for (var k = i; k < 0; k--) {
+	for (var k = i; k < drawTree; k++) {
 		space += ' ';
 	}
 
